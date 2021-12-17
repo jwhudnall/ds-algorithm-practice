0681e2ea323e0c23bc5554f921aa2345c7008572
James Hudnall <jimmyhudnall@gmail.com>
2021-12-17 11:38:18 -0500

    Add additional test for twoSum()

--- Jasmine/jasmine-standalone-3.10.1/spec/EasySpec.js
+++ Jasmine/jasmine-standalone-3.10.1/spec/EasySpec.js
@@ -1,7 +1,12 @@
 describe('twoSum', () => {
-  it('Returns the correct indices', () => {
+  it('Returns the correct indices for [2,7,11,15]', () => {
     var actual = twoSum([2,7,11,15], 9);
     var expected = [0, 1];
+    expect(actual).toEqual(expected);
+  });
+  it('Returns the correct indices for [3,2,4]', () => {
+    var actual = twoSum([3,2,4], 6);
+    var expected = [1,2];
     expect(actual).toEqual(expected);
   })
 })

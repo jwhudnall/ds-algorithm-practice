fea5b1b65b4fa0336b9563efa868badba7e731d7
James Hudnall <jimmyhudnall@gmail.com>
2021-12-17 13:53:56 -0500

    Implement isValid()

--- Jasmine/jasmine-standalone-3.10.1/src/Easy.js
+++ Jasmine/jasmine-standalone-3.10.1/src/Easy.js
@@ -15,3 +15,29 @@ var twoSum = function(nums, target) {
   }
   return [];
 };
+
+// Valid Parentheses - https://leetcode.com/problems/valid-parentheses/
+var isValid = function(s) {
+  var stack = [];
+  var opening = ['(', '[', '{'];
+  const bracketPairs = new Map([
+    [')', '('],
+    [']', '['],
+    ['}', '{']
+  ]);
+
+  for (let i = 0; i < s.length; i++) {
+    var char = s[i];
+    var stackTop = stack[stack.length - 1];
+
+    if (opening.includes(char)) {
+      stack.push(char);
+    } else if(bracketPairs.get(char) === stackTop && stack.length !== 0) {
+     stack.pop();
+   } else {
+     return false;
+   }
+  }
+
+  return stack.length === 0;
+};

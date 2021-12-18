1354c37ae8b7178f40519897a2f8ce7f9a913c5e
James Hudnall <jimmyhudnall@gmail.com>
2021-12-18 11:10:58 -0500

    Implement longestCommonPrefix

--- LeetCode/easy.js
+++ LeetCode/easy.js
@@ -2,14 +2,14 @@
 
 // Two Sum - https://leetcode.com/problems/two-sum/
 // Notes: Solution uses a hash map to reduce complexity to N.
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
   const hash = {};
 
   for (let i = 0; i < nums.length; i++) {
     const val = nums[i];
     const need = target - val;
     if (hash.hasOwnProperty(need)) {
-      return [ hash[need], i ];
+      return [hash[need], i];
     }
     hash[val] = i;
   }
@@ -17,7 +17,7 @@ var twoSum = function(nums, target) {
 };
 
 // Valid Parentheses - https://leetcode.com/problems/valid-parentheses/
-var isValid = function(s) {
+var isValid = function (s) {
   var stack = [];
   var opening = ['(', '[', '{'];
   const bracketPairs = new Map([
@@ -32,26 +32,26 @@ var isValid = function(s) {
 
     if (opening.includes(char)) {
       stack.push(char);
-    } else if(bracketPairs.get(char) === stackTop && stack.length !== 0) {
-     stack.pop();
-   } else {
-     return false;
-   }
+    } else if (bracketPairs.get(char) === stackTop && stack.length !== 0) {
+      stack.pop();
+    } else {
+      return false;
+    }
   }
 
   return stack.length === 0;
 };
 
 // Roman to Integer - https://leetcode.com/problems/roman-to-integer/
-var romanToInt = function(s) {
+var romanToInt = function (s) {
   const values = {
-    'I' : 1,
-    'V' : 5,
-    'X' : 10,
-    'L' : 50,
-    'C' : 100,
-    'D' : 500,
-    'M' : 1000
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
   };
 
   let total = 0;
@@ -72,6 +72,23 @@ var romanToInt = function(s) {
 };
 
 // Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
-var longestCommonPrefix = function(strs) {
+var longestCommonPrefix = function (strs) {
+  // Use double loop to compare all chars of first word with all chars of others
 
+  // Handle empty array, array with 1 word
+  let prefix = '';
+  if (strs.length === 0 || strs.length === 1) {
+    return prefix;
+  }
+
+  for (let i = 0; i < strs[0].length; i++) {
+    let char = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== char) {
+        return prefix;
+      }
+    }
+    prefix += char;
+  }
+  return prefix;
 };

7d2b3c85febad64eb0997c9f8e7da53ffefdb546
James Hudnall <jimmyhudnall@gmail.com>
2022-01-06 16:16:16 -0500

    Add initial tests for deleteMiddle

--- Jasmine/jasmine-standalone-3.10.1/spec/MediumSpec.js
+++ Jasmine/jasmine-standalone-3.10.1/spec/MediumSpec.js
@@ -6,3 +6,22 @@ describe('productExceptSelf', () => {
     expect(productExceptSelf([-1,1,0,-3,3])).toEqual([0,0,9,0,0]);
   });
 });
+
+describe('deleteMiddle', () => {
+  beforeEach(() => {
+    function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+    }
+  })
+  it('Should return null for list of size n = 1', () => {
+    let first = new ListNode(1);
+    expect(deleteMiddle(first)).toBeFalsy(); // Should return empty
+  });
+  it('Should return null for list of size n = 2', () => {
+    let first = new ListNode(1);
+    let second = new ListNode(2);
+    first.next = second; // should return first node only
+    expect(deleteMiddle(first)).toBe(first)
+  }
+});
